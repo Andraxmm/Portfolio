dfa7edeb9fac9fc689bc0f95ee857a40638a2309
Andraxmm <117832184+Andraxmm@users.noreply.github.com>
2025-09-04 13:58:21 +0200

    Create MoviesPage.jsx
    feat: añadir página MoviesPage para el buscador de películas

---
+++ src/Projects/pages/MoviesPage.jsx
@@ -0,0 +1,10 @@
+import BuscadorPeliculas from "../Projects/BuscadorPeliculas"
+
+export default function MoviesPage() {
+  return (
+    <main className="container-p py-8">
+      <h1 className="text-2xl font-bold mb-4">Buscador de Películas</h1>
+      <BuscadorPeliculas />
+    </main>
+  )
+}
