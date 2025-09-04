e7f2c29068a770e78572c6b50aeaf432a7608f09
Andraxmm <117832184+Andraxmm@users.noreply.github.com>
2025-09-04 13:58:11 +0200

    Update main.jsx
    feat: configurar React Router en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import MoviesPage from './pages/MoviesPage.jsx'   // 👈 la crearás ahora
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Ruta principal: tu portfolio */}
+        <Route path="/" element={<App />} />
+
+        {/* Ruta extra: página de películas */}
+        <Route path="/peliculas" element={<MoviesPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
