83dbd547743dacbc4fa6284d10d56f9f4c6ebcdf
Andraxmm <117832184+Andraxmm@users.noreply.github.com>
2025-09-11 12:16:13 +0200

    Update MoviesPage.jsx
    fix: corrección de rutas

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import api from "../Projects/BuscadorPeliculas/api.js";
-import MovieCard from "../Projects/BuscadorPeliculas/MovieCard.jsx";
+import api from "../MiniApps/BuscadorPeliculas/Api.js";
+import MovieCard from "../MiniApps/BuscadorPeliculas/MovieCard.jsx";
 import { Link } from "react-router-dom";
 
 
