5f4f3de67e8ab593cb6827486ea050ab4d3141bb
Andraxmm <117832184+Andraxmm@users.noreply.github.com>
2025-09-12 13:45:04 +0200

    Update Api.js
    chore: se han añadido comentarios para explicar el código

--- src/MiniApps/BuscadorPeliculas/Api.js
+++ src/MiniApps/BuscadorPeliculas/Api.js
@@ -1,12 +1,17 @@
+//Define la URL base para todas las peticiones a la API de TMDB
 const BASE = "https://api.themoviedb.org/3";
+
+//Toma la clave secreta (API Key) desde el archivo .env 
 const KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+//path: es el fragmento final del endpoint que quieres consultar. Ejemplo: `/search/movie?query=batman`
+//signal: es una señal para cancelar la petición si es necesario
 export default async function api(path, signal) {
     const url = new URL(BASE + path);
-  url.searchParams.set("api_key", KEY);
-  url.searchParams.set("language", "es-ES");
+  url.searchParams.set("api_key", KEY);     //api_key: sin esto, TMDB rechaza la petición.
+  url.searchParams.set("language", "es-ES"); 
 
-  const res = await fetch(url, { signal });
-  if (!res.ok) throw new Error(`TMDB ${res.status}`);
-  return res.json();
+  const res = await fetch(url, { signal }); //hacemos la petición a la URL completa. El lsignal sirve para poder cancelar la petición con un AbortController
+  if (!res.ok) throw new Error(`TMDB ${res.status}`); //Si la respuesta no es OK (por ejemplo 404, 401...), lanza un error personalizado
+  return res.json(); //Convierte la respuesta a JSON (objeto JS) y la devuelve.
 }  
