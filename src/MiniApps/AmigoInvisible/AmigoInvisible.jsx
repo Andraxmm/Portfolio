93a66a0c7b1b99f94c9e56490128715f311308bc
Andraxmm <117832184+Andraxmm@users.noreply.github.com>
2025-10-27 17:02:53 +0100

    Update AmigoInvisible.jsx
    chore: pequeño div para visualizar card

--- src/MiniApps/AmigoInvisible/AmigoInvisible.jsx
+++ src/MiniApps/AmigoInvisible/AmigoInvisible.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 
 export default function AmigoInvisible() {
-  const [participantes, setParticipantes] = useState('');
-  const [asignaciones, setAsignaciones] = useState('');
+  const [participantes, setParticipantes] = useState([]);
+  const [asignaciones, setAsignaciones] = useState([]);
+
+  return (
+    <div className="p-3 bg-indigo-50 rounded-lg text-center">
+      Mini App Amigo Invisible
+    </div>
+  );
 }
