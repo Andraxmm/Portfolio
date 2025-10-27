722093fda06ed683091be9e7634559d771398292
Andraxmm <117832184+Andraxmm@users.noreply.github.com>
2025-10-27 16:57:03 +0100

    Create AmigoInvisible.jsx
    feat: amigo invisible

---
+++ src/MiniApps/AmigoInvisible/AmigoInvisible.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+
+export default function AmigoInvisible() {
+  const [participantes, setParticipantes] = useState('');
+  const [asignaciones, setAsignaciones] = useState('');
+}
