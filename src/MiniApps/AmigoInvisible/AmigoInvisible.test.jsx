33ddc4ca0017b326960f1c2d1f7569bd0f0dc7ae
Andraxmm <117832184+Andraxmm@users.noreply.github.com>
2025-11-04 09:16:33 +0100

    feat: test unitario
    feat: test unitario con Vitest / Jest + Testing Library, en app 'Amigo Invisible'. Comprobamos que al escribir un nombre en el input y hacer clic en el botón, el participante aparezca en la lista.

---
+++ src/MiniApps/AmigoInvisible/AmigoInvisible.test.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import { render, screen, fireEvent } from "@testing-library/react";
+import AmigoInvisible from './AmigoInvisible';
+import { expect } from "vitest";
+
+describe('AmigoInvisible Component,', () => { 
+    it("permite agregar un participasnte y mostrarlo en la lista", async() => {
+        render(<AmigoInvisible />);
+
+        const organizadorBtn = await screen.findByRole('button', {name: /ir a vista organizador/i });
+        fireEvent.click(organizadorBtn);
+
+        const input = screen.getByLabelText(/nombre del participante/i);
+        const agregarBtn = screen.getByRole('button', { name: /agregar participante/i});
+
+        fireEvent.change(input, { target: { value: "Adrián" } });
+        fireEvent.click(agregarBtn);
+
+        expect(screen. getByText('Adrián')).toBeInTheDocument();
+    });
+});
+
+
